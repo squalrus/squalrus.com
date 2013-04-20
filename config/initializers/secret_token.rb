bf3bd24c93ff063b0e2f1de49948206dfced6f9a
Chad Schulz <chad.awesome@gmail.com>
2013-04-20 12:32:21 -0500

    Clearing out Ruby -- going to use Nodejs baby!

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RailsinstallerDemo::Application.config.secret_token = 'a4c1dc6ebb7ac4c407b64320c0c51553d633a414b243bf27a177f54cfa77b34b96ecfb32e3c5806547c97324d48d5697196186db9b72a8970f7909c3880ed3b9'
