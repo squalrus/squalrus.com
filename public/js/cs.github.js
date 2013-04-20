04a05fb170e2ba3f206f95d5c71eb2045388322c
Chad Schulz <chad.awesome@gmail.com>
2013-04-20 16:43:25 -0500

    updated github js

--- public/js/cs.github.js
+++ public/js/cs.github.js
@@ -5,13 +5,46 @@
 
     // Variables
     var profileUrl = 'https://api.github.com/users/squalrus'
-        ,eventsUrl = 'https://api.github.com/users/squalrus/events';
+        ,eventsUrl = 'https://api.github.com/users/squalrus/events'
+        ,updateProfile;
+
+    updateProfile = function( data ){
+        var $el
+        ,html = [];
+
+        $el = $( '#github-profile' );
+
+        html.push( '<p>' + data.login + '</p>' );
+        html.push( '<img src="' + data.avatar_url + '">' );
+
+        $el.html( html.join( '' ) );
+    };
+
+    updateFeed = function( data ){
+        var $el
+        ,html = [];
+
+        $el = $( '#github-feed' );
+
+        data.forEach( function( value, index, array ){
+            html.push( '<p>' + value.actor.login + ' did a ' + value.type + '</p>' );
+        });
+
+
+        $el.html( html.join( '' ) );
+    };
 
     $.ajax({
         url: profileUrl
         ,success: function( data, status ){
-            var el = $( '#github-status' );
-            el.html( data.name );
+            updateProfile( data );
+        }
+    });
+
+    $.ajax({
+        url: eventsUrl
+        ,success: function( data, status ){
+            updateFeed( data );
         }
     });
 
