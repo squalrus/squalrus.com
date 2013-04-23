2fc3dcd401601f6691b5c030bfdb74af9f079cb5
Chad Schulz <chad.awesome@gmail.com>
2013-04-23 11:00:43 -0500

    Refactors adding events -- simplified and generalized

--- public/js/cs.github.js
+++ public/js/cs.github.js
@@ -7,30 +7,39 @@
     var profileUrl = 'https://api.github.com/users/squalrus'
         ,eventsUrl = 'https://api.github.com/users/squalrus/events'
         ,updateProfile
+        ,addEntry
         ,actionEvents = []
         ,html = [];
 
-    actionEvents[ 'PushEvent' ] = function( data ){
+    addEntry = function( data ){
         var payload = data.payload
-            ,repo   = data.repo;
+            ,repo   = data.repo
+            ,date = new Date( data.created_at );
 
-        html.push( '<div>pushed ' + payload.commits.length + ' commits to <a href="' + repo.url + '" target="_blank">' + repo.name + '</a></div>' );
+        html.push( '<div class="activity-entry github">' + actionEvents[ data.type ]( payload, repo ) + '<span class="activity-date">' + date + '</span></div>' );
     };
 
-    actionEvents[ 'WatchEvent' ] = function( data ){
-        html.push( 'watched' );
+    /**
+     * Format a PushEvent
+     */
+    actionEvents[ 'PushEvent' ] = function( payload, repo ){
+        return 'pushed <code>' + payload.commits.length + ' commits</code> to <a href="' + repo.url + '" target="_blank">' + repo.name + '</a>';
     };
 
-    actionEvents[ 'CreateEvent' ] = function( data ){
-        html.push( 'created' );
+    actionEvents[ 'WatchEvent' ] = function( payload, repo ){
+        return payload.action + ' watching <a href="' + repo.url + '" target="_blank">' + repo.name + '</a>';
     };
 
-    actionEvents[ 'IssueCommentEvent' ] = function( data ){
-        html.push( 'commented' );
+    actionEvents[ 'CreateEvent' ] = function( payload, repo ){
+        return 'created <code>' + payload.ref + ' ' + payload.ref_type + '</code> <a href="' + repo.url + '" target="_blank">' + repo.name + '</a>';
     };
 
-    actionEvents[ 'PullRequestEvent' ] = function( data ){
-        html.push( 'pull requested' );
+    actionEvents[ 'IssueCommentEvent' ] = function( payload, repo ){
+        return payload.action + 'comment on issue in repo <a href="' + repo.url + '" target="_blank">' + repo.name + '</a> <span>' + payload.issue.body + '</span>';
+    };
+
+    actionEvents[ 'PullRequestEvent' ] = function( payload, repo ){
+        return 'pull requested';
     };
 
     updateProfile = function( data ){
@@ -51,7 +60,7 @@
         $el = $( '#github-feed' );
 
         data.forEach( function( value, index, array ){
-            actionEvents[ value.type ]( value );
+            addEntry( value );
         });
 
         $el.html( html.join( '' ) );
