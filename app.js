a05e3d6ef0b674a4bf863c932f320a2700bedccd
Chad Schulz <chad.awesome@gmail.com>
2013-04-28 23:45:00 -0500

    Add styling updates

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.set( 'view engine', 'ejs' );
 app.use( express.favicon() );
 app.use( express.logger('dev') );
 app.use( express.bodyParser() );
+app.use( express.compress() );
 app.use( express.methodOverride() );
 app.use( app.router );
 
