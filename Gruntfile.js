64415cf173738c481e96910088f9505a7d1597e4
Chad Schulz <chad.awesome@gmail.com>
2013-06-13 13:58:55 -0500

    Utilize Grunt for LESS compile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,8 @@ module.exports = function( grunt ){
     // Project Configuration
     grunt.initConfig({
          pkg: grunt.file.readJSON( 'package.json' )
+
+         // Uglify JS
         ,uglify: {
              options: { }
             ,dist: {
@@ -11,16 +13,32 @@ module.exports = function( grunt ){
                 }
             }
         }
+
+        // Compile LESS
+        ,less: {
+            development: {
+                options: {
+                    compress: true
+                }
+                ,files: {
+                     'public/css/cs.min.css': 'theme/cs.less'
+                    ,'public/css/cs.responsive.min.css': 'theme/cs.responsive.less'
+                }
+            }
+        }
+
+        // Watch Directories / Files
         ,watch: {
-             files: ['<% uglify.dist.files %>']
-            ,tasks: ['uglify']
+             files: ['lib/*.js', 'theme/*.less']
+            ,tasks: ['default']
         }
     });
 
     // Load the plugin that provides the 'uglify' task
     grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+    grunt.loadNpmTasks( 'grunt-contrib-less' );
     grunt.loadNpmTasks( 'grunt-contrib-watch' );
 
     // Default tasks
-    grunt.registerTask( 'default', ['uglify'] );
+    grunt.registerTask( 'default', ['uglify', 'less'] );
 };
